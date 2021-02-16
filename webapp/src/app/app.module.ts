b8a230128321c07a9ba15f304d3642c10c8dbb94
PhillipNinan <ninan.phillip@gmail.com>
2021-02-15 21:43:06 -0500

    init amplify auth

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -4,13 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// aws
+import {AmplifyUIAngularModule} from "@aws-amplify/ui-angular";
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from '../aws-exports';
+Amplify.configure(awsconfig);
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AmplifyUIAngularModule
   ],
   providers: [],
   bootstrap: [AppComponent]
